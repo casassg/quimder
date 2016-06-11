2124e448e8bc01ce59d92cffeebe8f06c798cea5
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2016-06-12 01:06:22 +0200

    well, why not?

--- app/src/main/java/tindre/love/quim/quimtindre/MainActivity.java
+++ app/src/main/java/tindre/love/quim/quimtindre/MainActivity.java
@@ -2,46 +2,71 @@ package tindre.love.quim.quimtindre;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
-import com.andtinder.model.CardModel;
-import com.andtinder.view.CardContainer;
-import com.andtinder.view.SimpleCardStackAdapter;
-import com.google.firebase.storage.FirebaseStorage;
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private CardContainer mCardContainer;
+    private ArrayList<String> al;
+    private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //add the view via xml or programmatically
+        SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView) findViewById(R.id.frame);
 
-        mCardContainer = (CardContainer) findViewById(R.id.layoutview);
+        al = new ArrayList<String>();
+        al.add("php");
+        al.add("c");
+        al.add("python");
+        al.add("java");
 
-        FirebaseStorage storage = FirebaseStorage.getInstance();
+        //choose your favorite adapter
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al);
 
-        SimpleCardStackAdapter adapter = new SimpleCardStackAdapter(this);
-        CardModel card1 = new CardModel("Title1", "Description goes here", getResources().getDrawable(R.drawable.picture1));
-        card1.setOnCardDimissedListener(new CardModel.OnCardDimissedListener() {
+        //set the listener and the adapter
+        flingContainer.setAdapter(arrayAdapter);
+        flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
             @Override
-            public void onLike() {
-                Toast.makeText(MainActivity.this, "LIKEEE", Toast.LENGTH_SHORT).show();
+            public void removeFirstObjectInAdapter() {
+                // this is the simplest way to delete an object from the Adapter (/AdapterView)
+                Log.d("LIST", "removed object!");
+                al.remove(0);
+                arrayAdapter.notifyDataSetChanged();
             }
 
             @Override
-            public void onDislike() {
-                Toast.makeText(MainActivity.this, "NOOOOO, SO UGLYY", Toast.LENGTH_SHORT).show();
+            public void onLeftCardExit(Object dataObject) {
+                //Do something on the left!
+                //You also have access to the original object.
+                //If you want to use it just cast it (String) dataObject
+                Toast.makeText(getApplicationContext(), "Left!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onRightCardExit(Object dataObject) {
+                Toast.makeText(getApplicationContext(), "Right!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdapterAboutToEmpty(int itemsInAdapter) {
+                // Ask for more data here
+                arrayAdapter.notifyDataSetChanged();
+                Log.d("LIST", "notified");
+            }
+
+            @Override
+            public void onScroll(float v) {
+
             }
         });
-        adapter.add(card1);
-        adapter.add(card1);
-        adapter.add(card1);
-        adapter.add(card1);
-        adapter.add(card1);
-        adapter.add(card1);
-        adapter.add(card1);
-        mCardContainer.setAdapter(adapter);
+
     }
 }
