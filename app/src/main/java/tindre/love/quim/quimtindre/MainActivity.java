d9df83de9be838234acb4c987aa658859c9cabc5
Gerard Casas <casassg@users.noreply.github.com>
2016-06-12 03:17:53 +0200

    New library (#1)
    * well, why not?
    
    * change to new library completed and Firebase added

--- app/src/main/java/tindre/love/quim/quimtindre/MainActivity.java
+++ app/src/main/java/tindre/love/quim/quimtindre/MainActivity.java
@@ -2,46 +2,83 @@ package tindre.love.quim.quimtindre;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.andtinder.model.CardModel;
-import com.andtinder.view.CardContainer;
-import com.andtinder.view.SimpleCardStackAdapter;
-import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+
+import tindre.love.quim.quimtindre.adapter.FirebaseAdapter;
+import tindre.love.quim.quimtindre.model.Felicitacio;
 
 public class MainActivity extends AppCompatActivity {
 
-    private CardContainer mCardContainer;
+    private DatabaseReference mDatabase;
+    private FirebaseAdapter<Felicitacio> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        mCardContainer = (CardContainer) findViewById(R.id.layoutview);
+        //add the view via xml or programmatically
+        SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView) findViewById(R.id.frame);
 
-        FirebaseStorage storage = FirebaseStorage.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("felicitacions");
+        mDatabase.push().setValue(new Felicitacio("TEST"));
 
-        SimpleCardStackAdapter adapter = new SimpleCardStackAdapter(this);
-        CardModel card1 = new CardModel("Title1", "Description goes here", getResources().getDrawable(R.drawable.picture1));
-        card1.setOnCardDimissedListener(new CardModel.OnCardDimissedListener() {
+
+        //choose your favorite adapter
+        adapter = new FirebaseAdapter<Felicitacio>(mDatabase,Felicitacio.class,R.layout.tinder,this) {
             @Override
-            public void onLike() {
-                Toast.makeText(MainActivity.this, "LIKEEE", Toast.LENGTH_SHORT).show();
+            protected void populateView(View v, Felicitacio model) {
+                TextView descripcioView = (TextView) v.findViewById(R.id.descripcio);
+                descripcioView.setText(model.text);
+            }
+        };
+
+
+        flingContainer.setAdapter(adapter);
+        flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
+            @Override
+            public void removeFirstObjectInAdapter() {
+                // this is the simplest way to delete an object from the Adapter (/AdapterView)
+                Log.d("LIST", "removed object!");
+                adapter.removeFirst();
             }
 
             @Override
-            public void onDislike() {
-                Toast.makeText(MainActivity.this, "NOOOOO, SO UGLYY", Toast.LENGTH_SHORT).show();
+            public void onLeftCardExit(Object dataObject) {
+                //Do something on the left!
+                //You also have access to the original object.
+                //If you want to use it just cast it (String) dataObject
+                Toast.makeText(getApplicationContext(), "Left!", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onRightCardExit(Object dataObject) {
+                Toast.makeText(getApplicationContext(), "Right!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdapterAboutToEmpty(int itemsInAdapter) {
+                // Ask for more data here
+                adapter.notifyDataSetChanged();
+                Log.d("LIST", "notified");
+            }
+
+            @Override
+            public void onScroll(float v) {
+
             }
         });
-        adapter.add(card1);
-        adapter.add(card1);
-        adapter.add(card1);
-        adapter.add(card1);
-        adapter.add(card1);
-        adapter.add(card1);
-        adapter.add(card1);
-        mCardContainer.setAdapter(adapter);
     }
+
 }
