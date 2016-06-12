a49f9b08c75bb421e57c99dc24b179e3da9f0096
albert <albert.suarez.molgo@est.fib.upc.edu>
2016-06-12 20:11:32 +0200

    refactor model

--- app/src/main/java/tindre/love/quim/quimtindre/activity/MainActivity.java
+++ app/src/main/java/tindre/love/quim/quimtindre/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package tindre.love.quim.quimtindre;
+package tindre.love.quim.quimtindre.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -12,13 +12,14 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 
+import tindre.love.quim.quimtindre.R;
 import tindre.love.quim.quimtindre.adapter.FirebaseAdapter;
-import tindre.love.quim.quimtindre.model.Felicitacio;
+import tindre.love.quim.quimtindre.model.GreetingCard;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DatabaseReference mDatabase;
-    private FirebaseAdapter<Felicitacio> adapter;
+    private DatabaseReference mGreetingCardDatabase;
+    private FirebaseAdapter<GreetingCard> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,57 +28,45 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //add the view via xml or programmatically
         SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView) findViewById(R.id.frame);
 
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("felicitacions");
-        mDatabase.push().setValue(new Felicitacio("TEST"));
+        mGreetingCardDatabase = FirebaseDatabase.getInstance().getReference().child("greetingCards");
 
-
-        //choose your favorite adapter
-        adapter = new FirebaseAdapter<Felicitacio>(mDatabase,Felicitacio.class,R.layout.tinder,this) {
+        adapter = new FirebaseAdapter<GreetingCard>(mGreetingCardDatabase, GreetingCard.class, R.layout.tinder, this) {
             @Override
-            protected void populateView(View v, Felicitacio model) {
-                TextView descripcioView = (TextView) v.findViewById(R.id.descripcio);
-                descripcioView.setText(model.text);
+            protected void populateView(View v, GreetingCard model) {
+                TextView viewDescription = (TextView) v.findViewById(R.id.description);
+                viewDescription.setText(model.getText());
             }
         };
-
 
         flingContainer.setAdapter(adapter);
         flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
             @Override
             public void removeFirstObjectInAdapter() {
-                // this is the simplest way to delete an object from the Adapter (/AdapterView)
-                Log.d("LIST", "removed object!");
                 adapter.removeFirst();
             }
 
             @Override
             public void onLeftCardExit(Object dataObject) {
-                //Do something on the left!
-                //You also have access to the original object.
-                //If you want to use it just cast it (String) dataObject
-                Toast.makeText(getApplicationContext(), "Left!", Toast.LENGTH_SHORT).show();
 
             }
 
             @Override
             public void onRightCardExit(Object dataObject) {
-                Toast.makeText(getApplicationContext(), "Right!", Toast.LENGTH_SHORT).show();
+
             }
 
             @Override
             public void onAdapterAboutToEmpty(int itemsInAdapter) {
-                // Ask for more data here
                 adapter.notifyDataSetChanged();
-                Log.d("LIST", "notified");
             }
 
             @Override
             public void onScroll(float v) {
 
             }
+
         });
     }
 
