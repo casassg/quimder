76270b170091428598549456c5e77d3dfab39648
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2016-06-12 03:00:46 +0200

    change to new library completed and Firebase added

--- app/src/main/java/tindre/love/quim/quimtindre/MainActivity.java
+++ app/src/main/java/tindre/love/quim/quimtindre/MainActivity.java
@@ -2,44 +2,55 @@ package tindre.love.quim.quimtindre;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 
-import java.util.ArrayList;
+import tindre.love.quim.quimtindre.adapter.FirebaseAdapter;
+import tindre.love.quim.quimtindre.model.Felicitacio;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<String> al;
-    private ArrayAdapter<String> arrayAdapter;
+    private DatabaseReference mDatabase;
+    private FirebaseAdapter<Felicitacio> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         //add the view via xml or programmatically
         SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView) findViewById(R.id.frame);
 
-        al = new ArrayList<String>();
-        al.add("php");
-        al.add("c");
-        al.add("python");
-        al.add("java");
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("felicitacions");
+        mDatabase.push().setValue(new Felicitacio("TEST"));
+
 
         //choose your favorite adapter
-        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al);
+        adapter = new FirebaseAdapter<Felicitacio>(mDatabase,Felicitacio.class,R.layout.tinder,this) {
+            @Override
+            protected void populateView(View v, Felicitacio model) {
+                TextView descripcioView = (TextView) v.findViewById(R.id.descripcio);
+                descripcioView.setText(model.text);
+            }
+        };
 
-        //set the listener and the adapter
-        flingContainer.setAdapter(arrayAdapter);
+
+        flingContainer.setAdapter(adapter);
         flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
             @Override
             public void removeFirstObjectInAdapter() {
                 // this is the simplest way to delete an object from the Adapter (/AdapterView)
                 Log.d("LIST", "removed object!");
-                al.remove(0);
-                arrayAdapter.notifyDataSetChanged();
+                adapter.removeFirst();
             }
 
             @Override
@@ -48,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
                 //You also have access to the original object.
                 //If you want to use it just cast it (String) dataObject
                 Toast.makeText(getApplicationContext(), "Left!", Toast.LENGTH_SHORT).show();
+
             }
 
             @Override
@@ -58,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onAdapterAboutToEmpty(int itemsInAdapter) {
                 // Ask for more data here
-                arrayAdapter.notifyDataSetChanged();
+                adapter.notifyDataSetChanged();
                 Log.d("LIST", "notified");
             }
 
@@ -67,6 +79,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
     }
+
 }
---
+++ app/src/main/java/tindre/love/quim/quimtindre/model/Felicitacio.java
@@ -0,0 +1,21 @@
+package tindre.love.quim.quimtindre.model;
+
+/**
+ * Created by casassg on 12/06/16.
+ *
+ * @author casassg
+ */
+public class Felicitacio {
+    public String text;
+
+    public Felicitacio(String text){
+        this.text = text;
+    }
+
+    public Felicitacio() {}
+
+    @Override
+    public String toString() {
+        return text;
+    }
+}
