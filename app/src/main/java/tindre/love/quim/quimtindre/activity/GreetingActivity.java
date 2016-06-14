fc4c5c4cfb4e9a0682b40925f1c246603fbe7f17
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2016-06-14 16:11:17 +0200

    add image to view

--- app/src/main/java/tindre/love/quim/quimtindre/activity/GreetingActivity.java
+++ app/src/main/java/tindre/love/quim/quimtindre/activity/GreetingActivity.java
@@ -1,10 +1,12 @@
 package tindre.love.quim.quimtindre.activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -15,6 +17,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import tindre.love.quim.quimtindre.R;
 import tindre.love.quim.quimtindre.model.GreetingCard;
+import tindre.love.quim.quimtindre.utils.ImagesUtils;
 
 public class GreetingActivity extends AppCompatActivity {
 
@@ -27,10 +30,12 @@ public class GreetingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_greeting);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+// TODO: STILL NEED TO FIGURE THIS OUT
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Intent intent = getIntent();
-        if(!intent.hasExtra(USER_ID)){
-            Log.e(TAG,USER_ID+" extra is required to start this activity. FINISH IT!");
+        if (!intent.hasExtra(USER_ID)) {
+            Log.e(TAG, USER_ID + " extra is required to start this activity. FINISH IT!");
             finish();
             // K.O.
         }
@@ -38,19 +43,41 @@ public class GreetingActivity extends AppCompatActivity {
         DatabaseReference mGreetingCardDatabase = FirebaseDatabase.getInstance().getReference()
                 .child(MainActivity.GREETING_CARDS)
                 .child(userId);
+        ImagesUtils.getImage(new ImagesUtils.SetDrawableCallback() {
+            @Override
+            public void giveMeMyDrawable(Bitmap bitmap) {
+                ImageView appBarLayout = (ImageView) findViewById(R.id.user_image_detail);
+                appBarLayout.setImageBitmap(bitmap);
+            }
+        },getApplicationContext(),userId);
         mGreetingCardDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 GreetingCard card = dataSnapshot.getValue(GreetingCard.class);
-                TextView authorView = (TextView) findViewById(R.id.user_name);
-                assert authorView != null;
-                authorView.setText(card.getAuthor());
+                setFields(card);
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
+                Log.e(TAG, "OH NO THE DATABASE CRASHED!", databaseError.toException());
             }
         });
+    }
+
+    private void setFields(GreetingCard card) {
+        TextView authorView = (TextView) findViewById(R.id.user_name);
+        TextView ageView = (TextView) findViewById(R.id.user_age);
+        TextView descriptionView = (TextView) findViewById(R.id.user_description);
+        assert authorView != null;
+        assert ageView != null;
+        assert descriptionView != null;
+
+        authorView.setText(card.getAuthor());
+        ageView.setText(String.valueOf(card.getAge()));
+        descriptionView.setText(card.getDescription());
+
+
+        //And now do the tricky part
+
     }
 }
